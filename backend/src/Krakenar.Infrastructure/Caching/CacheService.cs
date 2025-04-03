ed9e094747ff0862dfff5053813f8fbfe01e7880
Francis Pion <francispion@hotmail.com>
2025-04-03 01:06:06 -0400

    infra

---
+++ backend/src/Krakenar.Infrastructure/Caching/CacheService.cs
@@ -0,0 +1,23 @@
+﻿using Krakenar.Contracts.Configurations;
+using Krakenar.Core.Caching;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace Krakenar.Infrastructure.Caching;
+
+public class CacheService : ICacheService
+{
+  private const string ConfigurationKey = "Configuration";
+
+  private readonly IMemoryCache _memoryCache;
+
+  public CacheService(IMemoryCache memoryCache)
+  {
+    _memoryCache = memoryCache;
+  }
+
+  public virtual ConfigurationModel? Configuration
+  {
+    get => _memoryCache.TryGetValue(ConfigurationKey, out ConfigurationModel? configuration) ? configuration : null;
+    set => _memoryCache.Set(ConfigurationKey, value);
+  }
+}
---
+++ backend/src/Krakenar.Infrastructure/DependencyInjectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Krakenar.Core.Caching;
+using Krakenar.Infrastructure.Caching;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Krakenar.Infrastructure;
+
+public static class DependencyInjectionExtensions
+{
+  public static IServiceCollection AddKrakenarInfrastructure(this IServiceCollection services)
+  {
+    return services
+      .AddMemoryCache()
+      .AddSingleton<ICacheService, CacheService>();
+  }
+}
