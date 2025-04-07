f416c21d0799eb2f8d39d606a7c19984de0e6c01
Francis Pion <francispion@hotmail.com>
2025-04-07 12:29:39 -0400

    realm

---
+++ backend/src/Krakenar.Core/Realms/Events/RealmUpdated.cs
@@ -0,0 +1,21 @@
+﻿using Krakenar.Core.Settings;
+using Krakenar.Core.Tokens;
+using Logitar.EventSourcing;
+
+namespace Krakenar.Core.Realms.Events;
+
+public record RealmUpdated : DomainEvent
+{
+  public Change<DisplayName>? DisplayName { get; set; }
+  public Change<Description>? Description { get; set; }
+
+  public Secret? Secret { get; set; }
+  public Change<Url>? Url { get; set; }
+
+  public UniqueNameSettings? UniqueNameSettings { get; set; }
+  public PasswordSettings? PasswordSettings { get; set; }
+  public bool? RequireUniqueEmail { get; set; }
+  public bool? RequireConfirmedAccount { get; set; }
+
+  public Dictionary<Identifier, string?> CustomAttributes { get; set; } = [];
+}
--- backend/src/Krakenar.Core/ValidationExtensions.cs
+++ backend/src/Krakenar.Core/ValidationExtensions.cs
@@ -11,14 +11,29 @@ public static class ValidationExtensions
     return ruleBuilder.SetValidator(new AllowedCharactersValidator<T>(allowedCharacters));
   }
 
+  public static IRuleBuilderOptions<T, string> Description<T>(this IRuleBuilder<T, string> ruleBuilder)
+  {
+    return ruleBuilder.NotEmpty();
+  }
+
   public static IRuleBuilderOptions<T, string> DisplayName<T>(this IRuleBuilder<T, string> ruleBuilder)
   {
     return ruleBuilder.NotEmpty().MaximumLength(Core.DisplayName.MaximumLength);
   }
 
+  public static IRuleBuilderOptions<T, string> Identifier<T>(this IRuleBuilder<T, string> ruleBuilder)
+  {
+    return ruleBuilder.NotEmpty().MaximumLength(Core.Identifier.MaximumLength).SetValidator(new IdentifierValidator<T>());
+  }
+
   public static IRuleBuilderOptions<T, string> Locale<T>(this IRuleBuilder<T, string> ruleBuilder)
   {
     return ruleBuilder.NotEmpty().MaximumLength(Localization.Locale.MaximumLength).SetValidator(new LocaleValidator<T>());
+  }
+
+  public static IRuleBuilderOptions<T, string> Slug<T>(this IRuleBuilder<T, string> ruleBuilder)
+  {
+    return ruleBuilder.NotEmpty().MaximumLength(Core.Slug.MaximumLength).SetValidator(new SlugValidator<T>());
   }
 
   public static IRuleBuilderOptions<T, string> Url<T>(this IRuleBuilder<T, string> ruleBuilder)
