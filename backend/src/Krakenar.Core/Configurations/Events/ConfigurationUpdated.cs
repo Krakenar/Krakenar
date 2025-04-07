c05507742b84ec05c47b3e07b02f0906d5ee9e9e
Francis Pion <francispion@hotmail.com>
2025-04-07 00:33:59 -0400

    test

---
+++ backend/src/Krakenar.Core/Configurations/Events/ConfigurationUpdated.cs
@@ -0,0 +1,14 @@
+﻿using Krakenar.Core.Settings;
+using Krakenar.Core.Tokens;
+using Logitar.EventSourcing;
+
+namespace Krakenar.Core.Configurations.Events;
+
+public record ConfigurationUpdated : DomainEvent
+{
+  public Secret? Secret { get; set; }
+
+  public UniqueNameSettings? UniqueNameSettings { get; set; }
+  public PasswordSettings? PasswordSettings { get; set; }
+  public LoggingSettings? LoggingSettings { get; set; }
+}
