523831420274ffb134a4c9c8a582273a27805df9
Francis Pion <francispion@hotmail.com>
2025-04-07 00:29:26 -0400

    tests

---
+++ backend/src/Krakenar.Core/Actors/ActorExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Krakenar.Contracts.Actors;
+using Logitar;
+using Logitar.EventSourcing;
+
+namespace Krakenar.Core.Actors;
+
+public static class ActorExtensions
+{
+  private const char Separator = ':';
+
+  public static ActorId GetActorId(this Actor actor)
+  {
+    if (actor.Type == ActorType.System)
+    {
+      throw new ArgumentException($"The actor type cannot be {ActorType.System}.", nameof(actor));
+    }
+    string encoded = Convert.ToBase64String(actor.Id.ToByteArray()).ToUriSafeBase64();
+    return new ActorId(string.Join(Separator, actor.Type, encoded));
+  }
+}
