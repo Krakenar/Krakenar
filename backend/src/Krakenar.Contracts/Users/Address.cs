3db6301e96fc16f8fd1649deeb5507f609602e48
Francis Pion <francispion@hotmail.com>
2025-04-08 12:09:35 -0400

    contracts

---
+++ backend/src/Krakenar.Contracts/Users/Address.cs
@@ -0,0 +1,33 @@
+﻿namespace Krakenar.Contracts.Users;
+
+public record Address : Contact, IAddress
+{
+  public string Street { get; set; }
+  public string Locality { get; set; }
+  public string? PostalCode { get; set; }
+  public string? Region { get; set; }
+  public string Country { get; set; }
+  public string Formatted { get; set; }
+
+  public Address()
+    : this(string.Empty, string.Empty, null, null, string.Empty, string.Empty)
+  {
+  }
+
+  public Address(IAddress address, string formatted)
+    : this(address.Street, address.Locality, address.PostalCode, address.Region, address.Country, formatted, address.IsVerified)
+  {
+    IsVerified = address.IsVerified;
+  }
+
+  public Address(string street, string locality, string? postalCode, string? region, string country, string formatted, bool isVerified = false)
+    : base(isVerified)
+  {
+    Street = street;
+    Locality = locality;
+    PostalCode = postalCode;
+    Region = region;
+    Country = country;
+    Formatted = formatted;
+  }
+}
---
+++ backend/src/Krakenar.Contracts/Users/Phone.cs
@@ -0,0 +1,26 @@
+﻿namespace Krakenar.Contracts.Users;
+
+public record Phone : Contact, IPhone
+{
+  public string? CountryCode { get; set; }
+  public string Number { get; set; }
+  public string? Extension { get; set; }
+  public string E164Formatted { get; set; }
+
+  public Phone() : this(null, string.Empty, null, string.Empty)
+  {
+  }
+
+  public Phone(IPhone phone, string e164Formatted) : this(phone.CountryCode, phone.Number, phone.Extension, e164Formatted, phone.IsVerified)
+  {
+    IsVerified = phone.IsVerified;
+  }
+
+  public Phone(string? countryCode, string number, string? extension, string e164Formatted, bool isVerified = false) : base(isVerified)
+  {
+    CountryCode = countryCode;
+    Number = number;
+    Extension = extension;
+    E164Formatted = e164Formatted;
+  }
+}
