0000f812e1c32f56bd15ca21821f588479a18433
Francis Pion <francispion@hotmail.com>
2025-04-09 12:56:19 -0400

    migrate

--- backend/src/Krakenar/Program.cs
+++ backend/src/Krakenar/Program.cs
@@ -1,6 +1,9 @@
-﻿using Krakenar.Core;
+﻿using Krakenar.Constants;
+using Krakenar.Core;
 using Krakenar.Core.Configurations.Commands;
+using Krakenar.Infrastructure.Commands;
 using Krakenar.Settings;
+using Microsoft.FeatureManagement;
 
 namespace Krakenar;
 
@@ -20,10 +23,18 @@ internal class Program
 
     using IServiceScope scope = application.Services.CreateScope();
 
+    IFeatureManager featureManager = application.Services.GetRequiredService<IFeatureManager>();
+    if (await featureManager.IsEnabledAsync(Features.MigrateDatabase))
+    {
+      MigrateDatabase migrateDatabase = new();
+      ICommandHandler<MigrateDatabase> migrationHandler = scope.ServiceProvider.GetRequiredService<ICommandHandler<MigrateDatabase>>();
+      await migrationHandler.HandleAsync(migrateDatabase);
+    }
+
     DefaultSettings defaults = DefaultSettings.Initialize(configuration);
     InitializeConfiguration initializeConfiguration = new(defaults.Locale, defaults.UniqueName, defaults.Password);
-    ICommandHandler<InitializeConfiguration> handler = scope.ServiceProvider.GetRequiredService<ICommandHandler<InitializeConfiguration>>();
-    await handler.HandleAsync(initializeConfiguration);
+    ICommandHandler<InitializeConfiguration> configurationHandler = scope.ServiceProvider.GetRequiredService<ICommandHandler<InitializeConfiguration>>();
+    await configurationHandler.HandleAsync(initializeConfiguration);
 
     application.Run();
   }
