9da44a6fd7174636cf7535d804e461cc22340e10
Francis Pion <francispion@hotmail.com>
2025-04-14 19:34:32 -0400

    web

---
+++ backend/src/Krakenar.Web/HttpContextExtensions.cs
@@ -0,0 +1,97 @@
+﻿using Krakenar.Contracts;
+using Krakenar.Contracts.Sessions;
+using Krakenar.Contracts.Users;
+using Microsoft.Extensions.Primitives;
+
+namespace Krakenar.Web;
+
+public static class HttpContextExtensions
+{
+  private const string SessionIdKey = "SessionId";
+  private const string SessionKey = "Session";
+  private const string UserKey = "User";
+
+  public static IReadOnlyCollection<CustomAttribute> GetSessionCustomAttributes(this HttpContext context)
+  {
+    List<CustomAttribute> customAttributes = new(capacity: 2)
+    {
+      new("AdditionalInformation", context.GetAdditionalInformation())
+    };
+
+    string? ipAddress = context.GetClientIpAddress();
+    if (ipAddress != null)
+    {
+      customAttributes.Add(new("IpAddress", ipAddress));
+    }
+
+    return customAttributes.AsReadOnly();
+  }
+  public static string GetAdditionalInformation(this HttpContext context)
+  {
+    return JsonSerializer.Serialize(context.Request.Headers);
+  }
+  public static string? GetClientIpAddress(this HttpContext context)
+  {
+    string? ipAddress = null;
+
+    if (context.Request.Headers.TryGetValue("X-Forwarded-For", out StringValues xForwardedFor))
+    {
+      ipAddress = xForwardedFor.Single()?.Split(':').First();
+    }
+    ipAddress ??= context.Connection.RemoteIpAddress?.ToString();
+
+    return ipAddress;
+  }
+
+  public static Session? GetSession(this HttpContext context) => context.GetItem<Session>(SessionKey);
+  public static User? GetUser(this HttpContext context) => context.GetItem<User>(UserKey);
+  public static T? GetItem<T>(this HttpContext context, object key) => context.Items.TryGetValue(key, out object? value) ? (T?)value : default;
+
+  public static void SetSession(this HttpContext context, Session? session) => context.SetItem(SessionKey, session);
+  public static void SetUser(this HttpContext context, User? user) => context.SetItem(UserKey, user);
+  public static void SetItem<T>(this HttpContext context, object key, T? value)
+  {
+    if (value is null)
+    {
+      context.Items.Remove(key);
+    }
+    else
+    {
+      context.Items[key] = value;
+    }
+  }
+
+  public static Guid? GetSessionId(this HttpContext context)
+  {
+    byte[]? bytes = context.Session.Get(SessionIdKey);
+
+    return bytes == null ? null : new Guid(bytes);
+  }
+  public static bool IsSignedIn(this HttpContext context) => context.GetSessionId().HasValue;
+  public static void SignIn(this HttpContext context, Session session)
+  {
+    context.Session.Set(SessionIdKey, session.Id.ToByteArray());
+
+    //if (session.RefreshToken != null)
+    //{
+    //  CookiesSettings cookiesSettings = context.RequestServices.GetRequiredService<CookiesSettings>();
+    //  CookieOptions options = new()
+    //  {
+    //    HttpOnly = cookiesSettings.RefreshToken.HttpOnly,
+    //    MaxAge = cookiesSettings.RefreshToken.MaxAge,
+    //    SameSite = cookiesSettings.RefreshToken.SameSite,
+    //    Secure = cookiesSettings.RefreshToken.Secure
+    //  };
+    //  context.Response.Cookies.Append(Cookies.RefreshToken, session.RefreshToken, options);
+    //} // TODO(fpion): Cookies
+
+    context.SetSession(session);
+    context.SetUser(session.User);
+  }
+  public static void SignOut(this HttpContext context)
+  {
+    context.Session.Clear();
+
+    //context.Response.Cookies.Delete(Cookies.RefreshToken); // TODO(fpion): Cookies
+  }
+}
