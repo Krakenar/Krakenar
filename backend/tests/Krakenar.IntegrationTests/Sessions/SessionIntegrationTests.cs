2a7d97f6da30593de02377ac29729059a666ef43
Francis Pion <francispion@hotmail.com>
2025-04-16 01:09:14 -0400

    integration tests

---
+++ backend/tests/Krakenar.IntegrationTests/Sessions/SessionIntegrationTests.cs
@@ -0,0 +1,11 @@
+﻿namespace Krakenar.Sessions;
+
+[Trait(Traits.Category, Categories.Integration)]
+public class SessionIntegrationTests : IntegrationTests
+{
+  public SessionIntegrationTests() : base()
+  {
+  }
+
+  // TODO(fpion): implement
+}
