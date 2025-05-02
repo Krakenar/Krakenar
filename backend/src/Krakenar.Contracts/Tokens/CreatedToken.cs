4b6b68af04b97074fb95a85559b0551e60a08385
Francis Pion <francispion@hotmail.com>
2025-05-02 09:26:54 -0400

    contracts

---
+++ backend/src/Krakenar.Contracts/Tokens/CreatedToken.cs
@@ -0,0 +1,15 @@
+﻿namespace Krakenar.Contracts.Tokens;
+
+public record CreatedToken
+{
+  public string Token { get; set; } = string.Empty;
+
+  public CreatedToken()
+  {
+  }
+
+  public CreatedToken(string token)
+  {
+    Token = token;
+  }
+}
