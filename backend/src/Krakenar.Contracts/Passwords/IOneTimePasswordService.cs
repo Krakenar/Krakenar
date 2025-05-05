3aad91768e8f0851206fdd7ad50ed9b3e0d60197
Francis Pion <francispion@hotmail.com>
2025-05-05 11:29:04 -0400

    otp.contracts

---
+++ backend/src/Krakenar.Contracts/Passwords/IOneTimePasswordService.cs
@@ -0,0 +1,8 @@
+﻿namespace Krakenar.Contracts.Passwords;
+
+public interface IOneTimePasswordService
+{
+  Task<OneTimePassword> CreateAsync(CreateOneTimePasswordPayload payload, CancellationToken cancellationToken = default);
+  Task<OneTimePassword?> ReadAsync(Guid id, CancellationToken cancellationToken = default);
+  Task<OneTimePassword?> ValidateAsync(Guid id, ValidateOneTimePasswordPayload payload, CancellationToken cancellationToken = default);
+}
---
+++ backend/src/Krakenar.Contracts/Passwords/ValidateOneTimePasswordPayload.cs
@@ -0,0 +1,7 @@
+﻿namespace Krakenar.Contracts.Passwords;
+
+public record ValidateOneTimePasswordPayload
+{
+  public string Password { get; set; } = string.Empty;
+  public List<CustomAttribute> CustomAttributes { get; set; } = [];
+}
