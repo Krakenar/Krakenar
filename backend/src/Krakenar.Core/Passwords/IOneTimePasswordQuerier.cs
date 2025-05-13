f223f92db91f59bed6b4f4ef0b199e9eeb3e4620
Francis Pion <francispion@hotmail.com>
2025-05-12 20:11:49 -0400

    Delete OTPs when deleting a user.

--- backend/src/Krakenar.Core/Passwords/IOneTimePasswordQuerier.cs
+++ backend/src/Krakenar.Core/Passwords/IOneTimePasswordQuerier.cs
@@ -1,9 +1,12 @@
-﻿using OneTimePasswordDto = Krakenar.Contracts.Passwords.OneTimePassword;
+﻿using Krakenar.Core.Users;
+using OneTimePasswordDto = Krakenar.Contracts.Passwords.OneTimePassword;
 
 namespace Krakenar.Core.Passwords;
 
 public interface IOneTimePasswordQuerier
 {
+  Task<IReadOnlyCollection<OneTimePasswordId>> FindIdsAsync(UserId userId, CancellationToken cancellationToken = default);
+
   Task<OneTimePasswordDto> ReadAsync(OneTimePassword oneTimePassword, CancellationToken cancellationToken = default);
   Task<OneTimePasswordDto?> ReadAsync(OneTimePasswordId id, CancellationToken cancellationToken = default);
   Task<OneTimePasswordDto?> ReadAsync(Guid id, CancellationToken cancellationToken = default);
