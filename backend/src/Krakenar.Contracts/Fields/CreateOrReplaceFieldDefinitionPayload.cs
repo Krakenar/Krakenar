e47b6f5dabb38cd0a9d5320500b70a0cbeb36054
Francis Pion <francispion@hotmail.com>
2025-05-15 17:18:53 -0400

    contracts

---
+++ backend/src/Krakenar.Contracts/Fields/CreateOrReplaceFieldDefinitionPayload.cs
@@ -0,0 +1,27 @@
+﻿namespace Krakenar.Contracts.Fields;
+
+public record CreateOrReplaceFieldDefinitionPayload
+{
+  public string FieldType { get; set; }
+
+  public bool IsInvariant { get; set; }
+  public bool IsRequired { get; set; }
+  public bool IsIndexed { get; set; }
+  public bool IsUnique { get; set; }
+
+  public string UniqueName { get; set; }
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+  public string? Placeholder { get; set; }
+
+  public CreateOrReplaceFieldDefinitionPayload() : this(string.Empty, string.Empty)
+  {
+  }
+
+  public CreateOrReplaceFieldDefinitionPayload(string uniqueName, string fieldType)
+  {
+    FieldType = fieldType;
+
+    UniqueName = uniqueName;
+  }
+}
---
+++ backend/src/Krakenar.Contracts/Fields/FieldDefinition.cs
@@ -0,0 +1,33 @@
+﻿namespace Krakenar.Contracts.Fields;
+
+public class FieldDefinition
+{
+  public Guid Id { get; set; }
+
+  public int Order { get; set; }
+
+  public FieldType FieldType { get; set; } = new();
+
+  public bool IsInvariant { get; set; }
+  public bool IsRequired { get; set; }
+  public bool IsIndexed { get; set; }
+  public bool IsUnique { get; set; }
+
+  public string UniqueName { get; set; }
+  public string? DisplayName { get; set; }
+  public string? Description { get; set; }
+  public string? Placeholder { get; set; }
+
+  public FieldDefinition() : this(string.Empty)
+  {
+  }
+
+  public FieldDefinition(string uniqueName)
+  {
+    UniqueName = uniqueName;
+  }
+
+  public override bool Equals(object obj) => obj is FieldDefinition field && field.Id == Id;
+  public override int GetHashCode() => Id.GetHashCode();
+  public override string ToString() => $"{DisplayName ?? UniqueName} | {GetType()} (Id={Id})";
+}
