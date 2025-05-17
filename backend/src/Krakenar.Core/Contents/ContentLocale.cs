232d851b487ce03b22ff595259d32e495411062f
Francis Pion <francispion@hotmail.com>
2025-05-17 16:44:15 -0400

    fix

--- backend/src/Krakenar.Core/Contents/ContentLocale.cs
+++ backend/src/Krakenar.Core/Contents/ContentLocale.cs
@@ -2,4 +2,18 @@
 
 namespace Krakenar.Core.Contents;
 
-public record ContentLocale(UniqueName UniqueName, DisplayName? DisplayName, Description? Description, IReadOnlyDictionary<Guid, FieldValue> FieldValues);
+public record ContentLocale
+{
+  public UniqueName UniqueName { get; }
+  public DisplayName? DisplayName { get; }
+  public Description? Description { get; }
+  public IReadOnlyDictionary<Guid, FieldValue> FieldValues { get; }
+
+  public ContentLocale(UniqueName uniqueName, DisplayName? displayName = null, Description? description = null, IReadOnlyDictionary<Guid, FieldValue>? fieldValues = null)
+  {
+    UniqueName = uniqueName;
+    DisplayName = displayName;
+    Description = description;
+    FieldValues = fieldValues ?? new Dictionary<Guid, FieldValue>().AsReadOnly();
+  }
+}
