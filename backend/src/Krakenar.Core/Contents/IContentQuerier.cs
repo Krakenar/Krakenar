6b5a2b278683c36ce81c67b87935e5e97d9db8e8
Francis Pion <francispion@hotmail.com>
2025-05-18 23:33:10 -0400

    next

--- backend/src/Krakenar.Core/Contents/IContentQuerier.cs
+++ backend/src/Krakenar.Core/Contents/IContentQuerier.cs
@@ -1,10 +1,18 @@
-﻿using Krakenar.Core.Localization;
+﻿using Krakenar.Core.Fields;
+using Krakenar.Core.Localization;
 using ContentDto = Krakenar.Contracts.Contents.Content;
 
 namespace Krakenar.Core.Contents;
 
 public interface IContentQuerier
 {
+  Task<IReadOnlyDictionary<Guid, ContentId>> FindConflictsAsync(
+    ContentTypeId contentTypeId,
+    LanguageId? languageId,
+    ContentStatus status,
+    IReadOnlyDictionary<Guid, FieldValue> fieldValues,
+    ContentId contentId,
+    CancellationToken cancellationToken = default);
   Task<IReadOnlyDictionary<Guid, Guid>> FindContentTypeIdsAsync(IEnumerable<Guid> contentIds, CancellationToken cancellationToken = default);
   Task<ContentId?> FindIdAsync(ContentTypeId contentTypeId, LanguageId? languageId, UniqueName uniqueName, CancellationToken cancellationToken = default);
   Task<IReadOnlyCollection<ContentId>> FindIdsAsync(ContentTypeId contentTypeId, CancellationToken cancellationToken = default);
