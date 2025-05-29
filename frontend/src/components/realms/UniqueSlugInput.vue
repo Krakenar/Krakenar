72750b87cff570495ba04cada045fd6dc1ccfae9
Francis Pion <francispion@hotmail.com>
2025-05-28 21:25:49 -0400

    Fixed Realm creation. (#147)

--- frontend/src/components/realms/UniqueSlugInput.vue
+++ frontend/src/components/realms/UniqueSlugInput.vue
@@ -1,21 +1,16 @@
 <script setup lang="ts">
-import { TarCheckbox } from "logitar-vue3-ui";
-import { ref, watch } from "vue";
-import { stringUtils } from "logitar-js";
 import { useI18n } from "vue-i18n";
 
 import FormInput from "@/components/forms/FormInput.vue";
 
-const { slugify } = stringUtils;
 const { t } = useI18n();
 
-const props = withDefaults(
+withDefaults(
   defineProps<{
     id?: string;
     label?: string;
     max?: number | string;
     modelValue?: string;
-    nameValue?: string;
     required?: boolean | string;
   }>(),
   {
@@ -25,33 +20,13 @@ const props = withDefaults(
   },
 );
 
-const isCustom = ref<boolean>(props.modelValue !== slugify(props.nameValue));
-
-const emit = defineEmits<{
+defineEmits<{
   (e: "update:model-value", value: string): void;
 }>();
-
-function onCustomUpdate(value: boolean): void {
-  isCustom.value = value;
-  if (!value) {
-    emit("update:model-value", slugify(props.nameValue));
-  }
-}
-
-watch(
-  () => props.nameValue,
-  (name) => {
-    if (!isCustom.value) {
-      emit("update:model-value", slugify(name));
-    }
-  },
-  { immediate: true },
-);
 </script>
 
 <template>
   <FormInput
-    :disabled="!isCustom"
     :id="id"
     :label="t(label)"
     :max="max"
@@ -59,9 +34,5 @@ watch(
     :required="required"
     :rules="{ slug: true }"
     @update:model-value="$emit('update:model-value', $event)"
-  >
-    <template #after>
-      <TarCheckbox :id="`${id}-custom`" :label="t('realms.uniqueSlug.custom')" :model-value="isCustom" @update:model-value="onCustomUpdate" />
-    </template>
-  </FormInput>
+  />
 </template>
