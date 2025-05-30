c825b4b2a96aa7db551eddb7528eb19d728768de
Francis Pion <francispion@hotmail.com>
2025-05-30 15:26:02 -0400

    db

---
+++ backend/src/Krakenar.EntityFrameworkCore.Relational/Configurations/LogEventConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Krakenar.EntityFrameworkCore.Relational.Entities;
+using Logitar.EventSourcing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Krakenar.EntityFrameworkCore.Relational.Configurations;
+
+public sealed class LogEventConfiguration : IEntityTypeConfiguration<LogEvent>
+{
+  public void Configure(EntityTypeBuilder<LogEvent> builder)
+  {
+    builder.ToTable(KrakenarDb.LogEvents.Table.Table ?? string.Empty, KrakenarDb.LogEvents.Table.Schema);
+    builder.HasKey(x => x.LogEventId);
+
+    builder.HasIndex(x => new { x.LogId, x.EventId }).IsUnique();
+    builder.HasIndex(x => x.LogUid);
+
+    builder.Property(x => x.EventId).HasMaxLength(EventId.MaximumLength);
+
+    builder.HasOne(x => x.Log).WithMany(x => x.Events)
+      .HasPrincipalKey(x => x.LogId).HasForeignKey(x => x.LogId)
+      .OnDelete(DeleteBehavior.Cascade);
+  }
+}
---
+++ backend/src/Krakenar.EntityFrameworkCore.Relational/Configurations/LogExceptionConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Krakenar.EntityFrameworkCore.Relational.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Krakenar.EntityFrameworkCore.Relational.Configurations;
+
+public sealed class LogExceptionConfiguration : IEntityTypeConfiguration<LogException>
+{
+  public void Configure(EntityTypeBuilder<LogException> builder)
+  {
+    builder.ToTable(KrakenarDb.LogExceptions.Table.Table ?? string.Empty, KrakenarDb.LogExceptions.Table.Schema);
+    builder.HasKey(x => x.LogExceptionId);
+
+    builder.HasIndex(x => x.Id).IsUnique();
+    builder.HasIndex(x => x.LogUid);
+    builder.HasIndex(x => x.Type);
+
+    builder.Property(x => x.Type).HasMaxLength(byte.MaxValue);
+
+    builder.HasOne(x => x.Log).WithMany(x => x.Exceptions)
+      .HasPrincipalKey(x => x.LogId).HasForeignKey(x => x.LogId)
+      .OnDelete(DeleteBehavior.Cascade);
+  }
+}
