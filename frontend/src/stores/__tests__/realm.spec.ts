6b860ebca2df0664ac3c5727adf4782534600abc
Francis Pion <francispion@hotmail.com>
2025-05-31 17:49:52 -0400

    Completed Realm Store Unit Tests. (#168)

--- frontend/src/stores/__tests__/realm.spec.ts
+++ frontend/src/stores/__tests__/realm.spec.ts
@@ -63,4 +63,45 @@ describe("realmStore", () => {
     realmStore.exit();
     expect(realmStore.currentRealm).toBeUndefined();
   });
+
+  it.concurrent("should not do anything when deleting a realm that is not stored", () => {
+    const realmStore = useRealmStore();
+    realmStore.deleteRealm(realm);
+    expect(realmStore.currentRealm).toBeUndefined();
+    expect(realmStore.realms).toEqual([]);
+  });
+
+  it.concurrent("should remove a deleted realm from the store", () => {
+    const realmStore = useRealmStore();
+    realmStore.currentRealm = realm;
+    expect(realmStore.currentRealm).toBeDefined();
+    realmStore.realms.push(realm);
+    expect(realmStore.realms.length).toBe(1);
+    realmStore.deleteRealm(realm);
+    expect(realmStore.currentRealm).toBeUndefined();
+    expect(realmStore.realms).toEqual([]);
+  });
+
+  it.concurrent("should save a new realm into the store", () => {
+    const realmStore = useRealmStore();
+    expect(realmStore.currentRealm).toBeUndefined();
+    expect(realmStore.realms).toEqual([]);
+    realmStore.saveRealm(realm);
+    expect(realmStore.currentRealm).toBeUndefined();
+    expect(realmStore.realms.length).toBe(1);
+  });
+
+  it("should save an existing realm in the store", () => {
+    const realmStore = useRealmStore();
+    realmStore.enter(realm);
+    expect(realmStore.currentRealm?.id).toBe(realm.id);
+    realmStore.realms.push(realm);
+    expect(realmStore.realms.length).toBe(1);
+
+    const otherRealm: Realm = { ...realm, uniqueSlug: "other-world" };
+    realmStore.saveRealm(otherRealm);
+    expect(realmStore.currentRealm?.id).toBe(otherRealm.id);
+    expect(realmStore.realms.length).toBe(1);
+    expect(realmStore.realms[0].uniqueSlug).toBe(otherRealm.uniqueSlug);
+  });
 });
