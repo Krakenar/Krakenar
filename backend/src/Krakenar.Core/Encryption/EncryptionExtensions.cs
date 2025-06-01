ce74c2a661cd5ee453a23ace9ad6f65a967985b4
Francis Pion <francispion@hotmail.com>
2025-06-01 13:46:16 -0400

    mov

---
+++ backend/src/Krakenar.Core/Encryption/EncryptionExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Krakenar.Contracts.Senders;
+using Krakenar.Core.Messages;
+using Krakenar.Core.Realms;
+using Krakenar.Core.Senders;
+using Krakenar.Core.Senders.Settings;
+using Sender = Krakenar.Core.Senders.Sender;
+using SenderDto = Krakenar.Contracts.Senders.Sender;
+using TemplateContent = Krakenar.Core.Templates.Content;
+
+namespace Krakenar.Core.Encryption;
+
+public static class EncryptionExtensions
+{
+  public static void DecryptSettings(this IEncryptionManager manager, SenderDto sender)
+  {
+    RealmId? realmId = sender.Realm is null ? null : new(sender.Realm.Id);
+    if (sender.SendGrid is not null)
+    {
+      sender.SendGrid.ApiKey = manager.Decrypt(new EncryptedString(sender.SendGrid.ApiKey), realmId);
+    }
+    if (sender.Twilio is not null)
+    {
+      sender.Twilio.AccountSid = manager.Decrypt(new EncryptedString(sender.Twilio.AccountSid), realmId);
+      sender.Twilio.AuthenticationToken = manager.Decrypt(new EncryptedString(sender.Twilio.AuthenticationToken), realmId);
+    }
+  }
+  public static SenderSettings DecryptSettings(this IEncryptionManager manager, Sender sender)
+  {
+    switch (sender.Provider)
+    {
+      case SenderProvider.SendGrid:
+        SendGridSettings sendGrid = (SendGridSettings)sender.Settings;
+        return new SendGridSettings(manager.Decrypt(new EncryptedString(sendGrid.ApiKey), sender.RealmId));
+      case SenderProvider.Twilio:
+        TwilioSettings twilio = (TwilioSettings)sender.Settings;
+        return new TwilioSettings(
+          manager.Decrypt(new EncryptedString(twilio.AccountSid), sender.RealmId),
+          manager.Decrypt(new EncryptedString(twilio.AuthenticationToken), sender.RealmId));
+      default:
+        throw new SenderProviderNotSupportedException(sender.Provider);
+    }
+  }
+
+  public static TemplateContent Encrypt(this IEncryptionManager manager, TemplateContent content, RealmId? realmId)
+  {
+    string text = manager.Encrypt(content.Text, realmId).Value;
+    return new TemplateContent(content.Type, text);
+  }
+
+  public static IReadOnlyDictionary<string, string> Encrypt(this IEncryptionManager manager, Variables variables, RealmId? realmId)
+  {
+    IReadOnlyDictionary<string, string> decrypted = variables.AsDictionary();
+    Dictionary<string, string> encrypted = new(capacity: decrypted.Count);
+    foreach (KeyValuePair<string, string> variable in decrypted)
+    {
+      encrypted[variable.Key] = manager.Encrypt(variable.Value, realmId).Value;
+    }
+    return encrypted.AsReadOnly();
+  }
+}
--- backend/src/Krakenar.Core/Senders/SenderExtensions.cs
+++ backend/src/Krakenar.Core/Senders/SenderExtensions.cs
@@ -1,43 +1,10 @@
 ﻿using Krakenar.Contracts.Senders;
-using Krakenar.Core.Encryption;
-using Krakenar.Core.Realms;
 using Krakenar.Core.Senders.Settings;
-using SenderDto = Krakenar.Contracts.Senders.Sender;
 
 namespace Krakenar.Core.Senders;
 
 public static class SenderExtensions
 {
-  public static void DecryptSettings(this IEncryptionManager encryptionManager, SenderDto sender)
-  {
-    RealmId? realmId = sender.Realm is null ? null : new(sender.Realm.Id);
-    if (sender.SendGrid is not null)
-    {
-      sender.SendGrid.ApiKey = encryptionManager.Decrypt(new EncryptedString(sender.SendGrid.ApiKey), realmId);
-    }
-    if (sender.Twilio is not null)
-    {
-      sender.Twilio.AccountSid = encryptionManager.Decrypt(new EncryptedString(sender.Twilio.AccountSid), realmId);
-      sender.Twilio.AuthenticationToken = encryptionManager.Decrypt(new EncryptedString(sender.Twilio.AuthenticationToken), realmId);
-    }
-  }
-  public static SenderSettings DecryptSettings(this IEncryptionManager encryptionManager, Sender sender)
-  {
-    switch (sender.Provider)
-    {
-      case SenderProvider.SendGrid:
-        SendGridSettings sendGrid = (SendGridSettings)sender.Settings;
-        return new SendGridSettings(encryptionManager.Decrypt(new EncryptedString(sendGrid.ApiKey), sender.RealmId));
-      case SenderProvider.Twilio:
-        TwilioSettings twilio = (TwilioSettings)sender.Settings;
-        return new TwilioSettings(
-          encryptionManager.Decrypt(new EncryptedString(twilio.AccountSid), sender.RealmId),
-          encryptionManager.Decrypt(new EncryptedString(twilio.AuthenticationToken), sender.RealmId));
-      default:
-        throw new SenderProviderNotSupportedException(sender.Provider);
-    }
-  }
-
   public static SenderKind GetSenderKind(this SenderSettings settings) => settings.Provider.GetSenderKind();
   public static SenderKind GetSenderKind(this SenderProvider provider) => provider switch
   {
