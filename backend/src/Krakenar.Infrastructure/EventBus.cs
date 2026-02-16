5429ec3874ed301f6eaae67aaa9b5b467daaacac
Francis Pion <francispion@hotmail.com>
2026-02-16 00:02:38 -0500

    EventBus now reports events.

---
+++ backend/src/Krakenar.Infrastructure/EventBus.cs
@@ -0,0 +1,21 @@
+﻿using Krakenar.Core.Logging;
+using Logitar.EventSourcing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Krakenar.Infrastructure;
+
+public class EventBus : Logitar.EventSourcing.Infrastructure.EventBus
+{
+  protected virtual ILoggingService? LoggingService { get; }
+
+  public EventBus(IServiceProvider serviceProvider) : base(serviceProvider)
+  {
+    LoggingService = serviceProvider.GetService<ILoggingService>();
+  }
+
+  public override Task PublishAsync(IEvent @event, CancellationToken cancellationToken)
+  {
+    LoggingService?.Report(@event);
+    return base.PublishAsync(@event, cancellationToken);
+  }
+}
