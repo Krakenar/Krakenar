22910eddd87e7b3179b8f13b2d52fd7a57963ff5
Francis Pion <francispion@hotmail.com>
2026-05-24 21:57:12 -0400

    infra

---
+++ backend/src/Krakenar.Infrastructure/Messages/Providers/SmtpProvider/SmtpProviderHandler.cs
@@ -0,0 +1,25 @@
+﻿using Krakenar.Core.Messages;
+using Krakenar.Core.Senders.Settings;
+using Krakenar.Core.Templates;
+using Logitar.Net.Mail;
+
+namespace Krakenar.Infrastructure.Messages.Providers.SmtpProvider;
+
+public class SmtpProviderHandler : IMessageHandler
+{
+  public SmtpProviderHandler(SmtpProviderSettings settings)
+  {
+    // TODO(fpion): implement
+  }
+
+  public void Dispose()
+  {
+    // TODO(fpion): implement
+  }
+
+  public virtual async Task<SendMailResult> SendAsync(Message message, Content body, CancellationToken cancellationToken)
+  {
+    MailMessage mailMessage = message.ToMailMessage(body);
+    throw new NotImplementedException(); // TODO(fpion): implement
+  }
+}
