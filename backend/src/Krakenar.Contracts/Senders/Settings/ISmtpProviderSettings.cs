eab2f0d561ba5727ccd007315755bab24ab2e845
Francis Pion <francispion@hotmail.com>
2026-05-24 21:48:43 -0400

    contracts

---
+++ backend/src/Krakenar.Contracts/Senders/Settings/ISmtpProviderSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace Krakenar.Contracts.Senders.Settings;
+
+public interface ISmtpProviderSettings
+{
+  string Host { get; }
+  ushort Port { get; }
+  string Username { get; }
+  string Password { get; }
+  // TODO(fpion): SSL/TLS
+}
---
+++ backend/src/Krakenar.Contracts/Senders/Settings/SmtpProviderSettings.cs
@@ -0,0 +1,26 @@
+﻿namespace Krakenar.Contracts.Senders.Settings;
+
+public record SmtpProviderSettings : ISmtpProviderSettings
+{
+  public string Host { get; set; }
+  public ushort Port { get; set; }
+  public string Username { get; set; }
+  public string Password { get; set; }
+
+  public SmtpProviderSettings() : this(string.Empty, 0, string.Empty, string.Empty)
+  {
+  }
+
+  [JsonConstructor]
+  public SmtpProviderSettings(string host, ushort port, string username, string password)
+  {
+    Host = host;
+    Port = port;
+    Username = username;
+    Password = password;
+  }
+
+  public SmtpProviderSettings(ISmtpProviderSettings smtp) : this(smtp.Host, smtp.Port, smtp.Username, smtp.Password)
+  {
+  }
+}
